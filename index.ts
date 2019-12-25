329d482cf136fd7fa7fabc1857457189c4ed8aea
alex <alex.lozan@labs42.io>
2019-12-25 15:17:43 +0200

    init

---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as http from 'http';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200);
+  response.end(`Hello world! From ${os.platform()}!`);
+});
+
+server.listen(8000, '0.0.0.0');
+
+server.on('error', async error => {
+  await fs.promises.writeFile(path.join(__dirname, 'server-error.log'), 'error' + JSON.stringify(error));
+  server.close();
+  process.exit();
+});
